dfef74e372209455f8bb5c7abacd469fab7223d2
Mikko Loponen <mikko.loponen@iki.fi>
2013-01-27 19:57:58 +0200

    Solved 1 and 2.

--- src/project_euler/core.clj
+++ src/project_euler/core.clj
@@ -1,6 +1,25 @@
 (ns project-euler.core)
 
-(defn foo
-  "I don't do a whole lot."
+(defn multiple-of-3-or-5?
   [x]
-  (println x "Hello, World!"))
+  (or (= 0 (mod x 3))
+      (= 0 (mod x 5))))
+
+(defn next-fib
+  [[a b]]
+  [(+ a b) a])
+
+(defn fibs
+  "Returns a lazy sequence of fibonacci numbers."
+  []
+  (map first (iterate next-fib [1 0])))
+
+(defn euler001
+  "Returns the sum of all the multiples of 3 or 5 below 1000."
+  []
+  (reduce + (filter multiple-of-3-or-5? (range 1000))))
+
+(defn euler002
+  "Finds the sum of even-valued fibonacci numbers below 4 000 000."
+  []
+  (reduce + (filter even? (take-while (partial > 4000000) (fibs)))))
--- test/project_euler/core_test.clj
+++ test/project_euler/core_test.clj
@@ -2,6 +2,18 @@
   (:use clojure.test
         project-euler.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest euler001-test
+  (testing "Euler 001"
+    (is (multiple-of-3-or-5? 15))
+    (is (not (multiple-of-3-or-5? 28)))
+    (is (multiple-of-3-or-5? 9))
+    (println "Problem 001: " (euler001))))
+
+(deftest euler002-test
+  (testing "Euler 002"
+    (is (= [3 2] (next-fib [2 1])))
+    (is (= [1 1 2 3 5 8 13 21]
+           (take 8 (fibs))))
+    (println "Problem 002: " (euler002))))
+
+
