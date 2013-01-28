434e08d2ffbf415ec9dfa98ea256500d12d17c3f
Mikko Loponen <mikko.loponen@iki.fi>
2013-01-28 11:14:26 +0200

    Update test/project_euler/core_test.clj
    Tests 4-8.

--- test/project_euler/core_test.clj
+++ test/project_euler/core_test.clj
@@ -23,3 +23,31 @@
     (is (= [5 7 13 29]
            (sort (common/prime-factors 13195))))
     (println "Problem 003: " (time (euler003)))))
+
+(deftest euler004-test
+  (testing "Euler 004"
+    (is (= 1984
+           (common/reverse-num 4891)))
+    (println "Problem 004: " (time (euler004)))))
+
+(deftest euler005-test
+  (testing "Euler 005"
+    (println "Problem 005: " (time (euler005)))))
+
+(deftest euler006-test
+  (testing "Euler 006"
+    (is (= 121
+           (common/square 11)))
+    (println "Problem 006: " (time (euler006)))))
+
+(deftest euler007-test
+  (testing "Euler 007"
+    (is (= 13
+           (nth common/lazy-primes 6)))
+    (println "Problem 007: " (time (euler007)))))
+
+(deftest euler008-test
+  (testing "Euler 008"
+    (is (= '(1 2 3 4 5)
+           (common/string-to-digits "12345")))
+    (println "Problem 008: " (time (euler008)))))
